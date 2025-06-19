49248869ef6bf1a7801d6f1cc076209c79db7cd6
Bohdan Koval <opoudyabsd@gmail.com>
2025-06-19 23:18:53 +0200

    Create footer spec file and footerPage file for it. Implement test cases for footer links functionality: 'SLT-26', 'SLT-25', 'SLT-28'

---
+++ pom/footer/footerPage.spec.ts
@@ -0,0 +1,11 @@
+import { expect, type Locator, type Page } from '@playwright/test';
+
+
+export class FooterPage {
+    readonly page: Page
+    readonly title: Locator
+    constructor(page: Page) {
+        this.page = page
+        this.title = page.locator('[data-test="title"]')
+    }
+}
---
+++ tests/footer.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+import { loginToLab } from './../utils/loginTo'
+import { FooterPage } from '../pom/footer/footerPage.spec';
+test.describe('Footer links', () => {
+    test.beforeEach(async ({ page }) => {
+        await loginToLab(page)
+    });
+    [
+        { socialMedia: "LinkedIn", link: 'https://www.linkedin.com/company/sauce-labs/', title: 'Sauce Labs | LinkedIn', locator: '[data-test="social-linkedin"]' },
+        { socialMedia: "Twitter | X", link: 'https://x.com/saucelabs', title: 'Sauce Labs (@saucelabs) / X', locator: '[data-test="social-twitter"]' },
+        { socialMedia: "Facebook", link: 'https://www.facebook.com/saucelabs', title: 'Sauce Labs | Facebook', locator: '[data-test="social-facebook"]' },
+    ].forEach(({ socialMedia, link, title, locator }) => {
+        test(`Verify that ${socialMedia} link in footer section redirects to the "Sauce Labs" LinkedIn page correctly`, async ({ page, context }) => {
+            const footerPage = new FooterPage(page)
+            await test.step('User on the products page', async () => {
+                await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+                await expect(footerPage.title).toHaveText('Products')
+            })
+            await test.step(`Click on the ${socialMedia} link image`, async () => {
+                const [newPage] = await Promise.all([
+                    context.waitForEvent('page'),
+                    page.locator(locator).click(),
+                ]); // Verify that new opened page is main page right now
+                await newPage.waitForLoadState();
+                await expect(newPage).toHaveURL(link)
+                await expect(newPage).toHaveTitle(title)
+            })
+        })
+    })
+
+
+})
